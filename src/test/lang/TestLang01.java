39950debfa12e3e8ba83e2760376ea4953fbc04c
王强 <wangqiang.wq@elecwatt.com>
2021-11-30 12:36:02 +0800

    fix: 提交测试

--- src/test/lang/TestLang01.java
+++ src/test/lang/TestLang01.java
@@ -1,11 +1,11 @@
-package test;
+package test.lang;
 
 /**
  * @Author: 世墨
- * @Date: 2021/11/30 10:58
+ * @Date: 2021/11/30 11:13
  * @DESCRIPTION
  */
-public class test {
+public class TestLang01 {
     public static void main(String[] args) {
         System.out.println("1");
     }

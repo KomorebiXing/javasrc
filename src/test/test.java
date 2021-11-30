c87dec0459a021eea115fd83bb572f7f64d92819
世墨 <1302999295@qq.com>
2021-11-30 11:04:18 +0800

    first commit

---
+++ src/test/test.java
@@ -0,0 +1,12 @@
+package test;
+
+/**
+ * @Author: 世墨
+ * @Date: 2021/11/30 10:58
+ * @DESCRIPTION
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("1");
+    }
+}
